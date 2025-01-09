003e3744fe8a69af140e476785be141bd1477f14
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2025-01-09 20:44:32 -0300

    .

--- main.py
+++ main.py
@@ -4,22 +4,27 @@ app = Flask(__name__)
 
 todo_list = []
 
-@app.route("/")
+@app.route("/") # ROTA PRINCIPAL
 def home():
     return render_template("index.html", todo_list=todo_list)
 
-@app.route("/add", methods=["POST"])
+@app.route("/add", methods=["POST"]) # ADICIONAR UMA NOVA TAREFA
 def add_task():
     task = request.form.get("task")
     if task:
         todo_list.append(task)
     return redirect(url_for("home"))
 
-@app.route("/delete/<int:task_id>", methods=["GET"])
+@app.route("/delete/<int:task_id>", methods=["GET"]) # DELETAR UMA TAREFA
 def delete_task(task_id):
     if 0 <= task_id < len(todo_list):
         todo_list.pop(task_id)
     return redirect(url_for("home"))
+
+@app.route("/update/<int:task_id>", methods=["POST"])
+def update_task(task_id):
+    if 0 <= task_id.pop(todo_list):
+        pass
 
 
 if __name__ == '__main__':
